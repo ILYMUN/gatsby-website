5f88b9ca2876726cc7768b427ce295ab98be1eac
Simon Roux <60037579+Simpos@users.noreply.github.com>
2021-05-25 21:51:02 +0200

    Add the hero2 part
    The hero2 part is a variant of the hero part, to implement the mini ilymun.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,6 +6,7 @@ import { Footer } from "../components/footer";
 
 import { HomeAboutSection } from "../components/home/about";
 import { LandingHero } from "../components/home/hero";
+import { LandginHero2 } from "../components/home/hero2";
 import { LandingParallax } from "../components/home/parallax";
 import { LandingValues } from "../components/home/values";
 import { LandingTeam } from "../components/home/team";
@@ -49,6 +50,7 @@ class HomePage extends React.Component {
         <div id="content" class="site-content">
           <LandingHero />
           <HomeAboutSection />
+          <LandingHero2 />
           <LandingParallax />
           <LandingValues />
           <LandingTeam />
