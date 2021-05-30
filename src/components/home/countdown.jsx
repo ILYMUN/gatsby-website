8e263b8d833a795ed5ec06960a77120dd51787b0
Simon Roux <60037579+Simpos@users.noreply.github.com>
2021-05-30 23:25:15 +0200

    Update countdown.jsx
    Update for debuging

--- src/components/home/countdown.jsx
+++ src/components/home/countdown.jsx
@@ -30,7 +30,8 @@ export class CountDownParallax extends Component {
 
   calculateCountdown(endDate) {
     let diff = (Date.parse(new Date(endDate)) - Date.parse(new Date())) / 1000;
-    console.log(diff);
+    //console.log(diff);
+    console.log(Date.parse(new Date(endDate)));
     // clear countdown when date is reached
     if (diff <= 0) return false;
 
