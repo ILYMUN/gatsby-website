cf9de148476321ea76f2724dd4a0d9d966119b51
Simon Roux <60037579+Simpos@users.noreply.github.com>
2021-06-04 18:18:48 +0200

    Update countdown.jsx

--- src/components/home/countdown.jsx
+++ src/components/home/countdown.jsx
@@ -32,7 +32,7 @@ export class CountDownParallax extends Component {
     let diff = (Date.parse(new Date(endDate)) - Date.parse(new Date())) / 1000;
     //console.log(diff);
     //console.log(Date.parse(new Date(endDate)));
-    console.log(endDate.toString());
+    console.log(new Date(endDate.toString()));
     
     // clear countdown when date is reached
     if (diff <= 0) return false;
@@ -204,7 +204,6 @@ export class CountDownParallax extends Component {
             date.eventStartHourUtc,
             date.eventStartMinuteUtc
           );
-          console.log(this.endDate);
           return countDownParallax;
         }}
       />
