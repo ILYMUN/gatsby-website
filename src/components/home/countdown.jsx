5bd3bd981e6ffb3733f2ff316c163f1dff5b9fea
Simon Roux <60037579+Simpos@users.noreply.github.com>
2021-06-04 18:24:13 +0200

    Update countdown.jsx

--- src/components/home/countdown.jsx
+++ src/components/home/countdown.jsx
@@ -6,7 +6,7 @@ export class CountDownParallax extends Component {
   constructor(props) {
     super(props);
 
-    this.endDate = new Date();
+    this.endDate = new Date("5 Jun 2021 15:23:23");
 
     this.state = {
       days: 0,
