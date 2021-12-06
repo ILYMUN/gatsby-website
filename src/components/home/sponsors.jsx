0c9235233d1596cb7a1e46d6e9dcd0db186738e5
Simon Roux <60037579+Simpos@users.noreply.github.com>
2021-12-06 15:22:01 +0100

    Update sponsors.jsx

--- src/components/home/sponsors.jsx
+++ src/components/home/sponsors.jsx
@@ -10,31 +10,15 @@ export class LandingSponsors extends Component {
         query={graphql`
           {
             content: markdownRemark(
-              frontmatter: { section: { eq: "team" }, page: { eq: "home" } }
+              frontmatter: { section: { eq: "sponsors" }, page: { eq: "home" } }
             ) {
               frontmatter {
                 title
                 subtitle
                 image1
-                name1
                 image2
-                name2
                 image3
-                name3
                 image4
-                name4
-                image5
-                name5
-                image6
-                name6
-                image7
-                name7
-                #portrait{
-                #  {
-                #    image
-                #    name
-                #  }
-                #}
               }
               
               html
@@ -43,7 +27,7 @@ export class LandingSponsors extends Component {
         `}
         
         render={data => (
-          <section id="team">
+          <section id="sponsors">
             <div>
               <div class="gray-bg angular section-padding">
                 <div class="top-angle"></div>
@@ -68,7 +52,6 @@ export class LandingSponsors extends Component {
                                 src={require("../../../static/images/team/" + data.content.frontmatter.image1)}
                               />
                             </LazyLoad>
-                            <p>{data.content.frontmatter.name1}</p>
                           </ScrollAnimation>
                         </div>
                         <div class="image_contenair col-md-4 col-sm-4">
@@ -78,7 +61,6 @@ export class LandingSponsors extends Component {
                                 src={require("../../../static/images/team/" + data.content.frontmatter.image2)}
                               />
                             </LazyLoad>
-                            <p>{data.content.frontmatter.name2}</p>
                           </ScrollAnimation>
                         </div>
 
@@ -90,7 +72,6 @@ export class LandingSponsors extends Component {
                                 src={require("../../../static/images/team/" + data.content.frontmatter.image3)}
                               />
                             </LazyLoad>
-                            <p>{data.content.frontmatter.name3}</p>
                           </ScrollAnimation>
                         </div>
                         <div class="image_contenair col-md-4 col-sm-4">
@@ -100,55 +81,9 @@ export class LandingSponsors extends Component {
                                 src={require("../../../static/images/team/" + data.content.frontmatter.image4)}
                               />
                             </LazyLoad>
-                            <p>{data.content.frontmatter.name4}</p>
-                          </ScrollAnimation>
-                        </div>
-                        <div class="image_contenair col-md-4 col-sm-4">
-                          <ScrollAnimation animateIn="bounceInUp" animateOnce>
-                            <LazyLoad>
-                              <img
-                                src={require("../../../static/images/team/" + data.content.frontmatter.image5)}
-                              />
-                            </LazyLoad>
-                            <p>{data.content.frontmatter.name5}</p>
-                          </ScrollAnimation>
-                        </div>
-
-
-                        <div class="image_contenair col-md-4 col-sm-4">
-                          <ScrollAnimation animateIn="bounceInUp" animateOnce>
-                            <LazyLoad>
-                              <img
-                                src={require("../../../static/images/team/" + data.content.frontmatter.image6)}
-                              />
-                            </LazyLoad>
-                            <p>{data.content.frontmatter.name6}</p>
-                          </ScrollAnimation>
-                        </div>
-                        <div class="image_contenair col-md-4 col-sm-4">
-                          <ScrollAnimation animateIn="bounceInUp" animateOnce>
-                            <LazyLoad>
-                              <img
-                                src={require("../../../static/images/team/" + data.content.frontmatter.image7)}
-                              />
-                            </LazyLoad>
-                            <p>{data.content.frontmatter.name7}</p>
                           </ScrollAnimation>
                         </div>
                       </div>
-                    </div>
-                    <div class="row">
-                      <ScrollAnimation animateIn="bounceInUp" animateOnce>
-                        <form action="/team/">
-                          <button
-                            type="submit"
-                            class="button"
-                            style={{ display: "block", margin: "0 auto" }}
-                          >
-                            See the Entire Team
-                          </button>
-                        </form>
-                      </ScrollAnimation>
                     </div>
                   </div>
                 </div>
