8ba0189a063a5ee302ff3f6b822f6972bf6e1e90
Pablo Gamito <pablo@gamito.me>
2021-12-18 23:46:58 +0100

    Add missing google analytics tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-44179721-5`,
       }
     },
     `gatsby-plugin-feed`,
